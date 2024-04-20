067b0910019ae1d6ebecf93c46631c33918fc15c
Joshua T Edwards <Joshua.tylor.edwards@gmail.com>
2024-04-20 00:39:15 -0500

    Add new game assets and update lighting settings
    This commit includes the addition of several new game assets such as particle effects, prefabs, sprites, materials, and artwork. The lighting settings for the DeadRevolver scene have also been updated. These additions and changes aim to improve the overall visual aesthetics and functionality of the game.

---
+++ Assets/JAssets/Scripts_SC/GameController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace JAssets.Scripts_SC
+{
+	public class GameController : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject playerPrefab; // set this in Inspector
+
+		void Start()
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				// Instantiate a new PlayerInput for each player
+				var playerInput = PlayerInput.Instantiate(playerPrefab);
+            
+				// Access the playerIndex property
+				int playerIndex = playerInput.playerIndex;
+
+				// Use the playerIndex for anything player-specific, like setting initial positions
+				playerInput.transform.position = GetInitialPlayerPosition(playerIndex);
+			}
+		}
+
+		Vector3 GetInitialPlayerPosition(int playerIndex)
+		{
+			// Return some position based on the player index
+			return Vector2.zero;
+		}
+	}
+}
